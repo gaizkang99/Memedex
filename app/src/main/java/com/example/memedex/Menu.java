c34a9b1bfb47cabf20b65864d47bff34cb765f65
uri <urielias8@gmail.com>
2022-03-14 16:59:51 +0000

    signin buttons and layout

---
+++ app/src/main/java/com/example/memedex/Menu.java
@@ -0,0 +1,18 @@
+package com.example.memedex;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Menu extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_opciones);
+
+    }
+
+}
--- app/src/main/java/com/example/memedex/SignIn.java
+++ app/src/main/java/com/example/memedex/SignIn.java
@@ -1,6 +1,9 @@
 package com.example.memedex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +12,28 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
+
+
+        Button signIn = (Button) findViewById(R.id.signIn);
+        Button back = (Button) findViewById(R.id.back);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSign = new Intent(SignIn.this, Menu.class);
+                startActivity(intentSign);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLogin = new Intent(SignIn.this, MainActivity.class);
+                startActivity(intentLogin);
+            }
+        });
+
+
+
     }
 }

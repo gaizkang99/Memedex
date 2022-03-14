25dcd68532265a25f4a895cc03ee1b8de15e7ae7
uri <urielias8@gmail.com>
2022-03-14 17:36:39 +0000

    signin & login buttons and layout + menu activity

--- app/src/main/java/com/example/memedex/Login.java
+++ app/src/main/java/com/example/memedex/Login.java
@@ -1,6 +1,9 @@
 package com.example.memedex;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,25 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        Button login = (Button) findViewById(R.id.logIn);
+        Button back = (Button) findViewById(R.id.back);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSign = new Intent(Login.this, Menu.class);
+                startActivity(intentSign);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLogin = new Intent(Login.this, MainActivity.class);
+                startActivity(intentLogin);
+            }
+        });
 
 
     }
